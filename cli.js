45d517dfff47080b9cdd48e151319382c49755d1
Eugene Sharygin <eush77@gmail.com>
2015-02-23 19:59:33 +0300

    Extract CLI specific code to a separate file

---
+++ cli.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+var onchange = require('./')
+
+// Parse argv with minimist...it's easier this way.
+var argv = require('minimist')(process.argv.slice(2), {
+  '--': true
+})
+
+// Print usage info
+if ( ! argv._.length || argv.help) {
+  console.log('Usage:  onchange [file]... -- <command> [arg]...')
+  process.exit()
+}
+
+// Setup some storage variables
+var arg
+var matches = argv._
+
+// Build exclusion list
+var excludes = []
+if (Array.isArray(argv.exclude)) {
+  excludes = argv.exclude
+} else if (argv.exclude) {
+  excludes = [argv.exclude]
+}
+
+excludes.forEach(function (exclude) {
+  matches.push('!' + exclude)
+})
+
+// Shift first thing after to command and use the rest as args
+var args = argv['--']
+var command = args.shift()
+
+// Start watcher
+onchange(matches, command, args)
--- index.js
+++ index.js
@@ -1,80 +1,50 @@
-#!/usr/bin/env node
 var spawn = require('cross-spawn').spawn
 var log = require('debug')('onchange')
 var chokidar = require('chokidar')
 
-// Parse argv with minimist...it's easier this way.
-var argv = require('minimist')(process.argv.slice(2), {
-  '--': true
-})
+module.exports = function (matches, command, args) {
+  var pwd = process.cwd()
 
-// Print usage info
-if ( ! argv._.length || argv.help) {
-  console.log('Usage:  onchange [file]... -- <command> [arg]...')
-  process.exit()
-}
+  // Notify the user what they are watching
+  log('watching ' + matches.join(', '))
 
-// Setup some storage variables
-var arg
-var pwd = process.cwd()
-var matches = argv._
+  // Ignore node_modules folders, as they eat CPU like crazy
+  matches.push('!**/node_modules/**')
 
-// Build exclusion list
-var excludes = []
-if (Array.isArray(argv.exclude)) {
-  excludes = argv.exclude
-} else if (argv.exclude) {
-  excludes = [argv.exclude]
-}
+  // Convert arguments to templates
+  var tmpls = args.map(tmpl)
 
-excludes.forEach(function (exclude) {
-  matches.push('!' + exclude)
-})
+  var watcher = chokidar.watch(matches)
+  watcher.on('ready', function () {
+    var running = false
 
-// Shift first thing after to command and use the rest as args
-var args = argv['--']
-var command = args.shift()
+    // For any change, creation or deletion, try to run.
+    // However, skip if the last run is still active.
+    watcher.on('all', function (event, file) {
+      if (running) return
+      running = true
 
-// Convert arguments to templates
-var tmpls = args.map(tmpl)
+      // Log the event and the file affected
+      log(event + ' ' + file.replace(pwd, ''))
 
-// Notify the user what they are watching
-log('watching ' + matches.join(', '))
+      // Generate argument strings from templates
+      var filtered = tmpls.map(function (tmpl) {
+        return tmpl({ changed: file })
+      })
 
-// Ignore node_modules folders, as they eat CPU like crazy
-matches.push('!**/node_modules/**')
+      // Run the command and forward output
+      var proc = spawn(command, filtered, {
+        stdio: ['ignore', process.stdout, process.stderr]
+      })
 
-// Start watcher
-var watcher = chokidar.watch(matches)
-watcher.on('ready', function () {
-  var running = false
-
-  // For any change, creation or deletion, try to run.
-  // However, skip if the last run is still active.
-  watcher.on('all', function (event, file) {
-    if (running) return
-    running = true
-
-    // Log the event and the file affected
-    log(event + ' ' + file.replace(pwd, ''))
-
-    // Generate argument strings from templates
-    var filtered = tmpls.map(function (tmpl) {
-      return tmpl({ changed: file })
-    })
-
-    // Run the command and forward output
-    var proc = spawn(command, filtered, {
-      stdio: ['ignore', process.stdout, process.stderr]
-    })
-
-    // Log the result and unlock
-    proc.on('close', function (code) {
-      log('completed with code ' + code)
-      running = false
+      // Log the result and unlock
+      proc.on('close', function (code) {
+        log('completed with code ' + code)
+        running = false
+      })
     })
   })
-})
+}
 
 //
 // Helpers
